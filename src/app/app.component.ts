70018dacf97102a933f11aa4a84b7e5fb0a4e68e
zivkoss <zivcoss@yahoo.com>
2023-05-29 13:46:26 +0200

    new code added in app.c.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ThisReceiver } from '@angular/compiler';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -40,9 +40,20 @@ export class AppComponent implements OnInit {
   private fetchPosts() {
     this.http
       .get('https://angular-http-basicsss-default-rtdb.firebaseio.com//posts.json')
-      .subscribe(posts => {
-        console.log(posts)
-      });
+      .pipe(
+         map(responseData => {
+           const postArray = [];
+           for (const key in  responseData) {
+             if (responseData.hasOwnProperty(key)) {
+                postArray.push({ ...responseData[key], id: key });
+             }
+           }
+           return postArray;
+         })
+        ) 
+        .subscribe(posts => {
+           console.log(posts)
+        });
 
     }    
 }   
