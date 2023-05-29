0c3b9049e4fc5fc4ad3e86d52569ff1195756925
zivkoss <zivcoss@yahoo.com>
2023-05-29 12:00:08 +0200

    'https://angular-http-basicsss-default-rtdb.firebaseio.com//posts.json'// aderss changed in app.c.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,11 +16,13 @@ export class AppComponent implements OnInit {
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     // console.log(postData);
-    this.http.post(
-      'https://github.com/zivkoss/angular-.http.basics.git/posts.json',
+    this.http
+      .post(
+      'https://angular-http-basicsss-default-rtdb.firebaseio.com//posts.json',
        postData
-    ).subscribe(responseDate => {
-      console.log(responseDate)
+    )
+    .subscribe(responseData => {
+      console.log(responseData);
     });
   }
 
