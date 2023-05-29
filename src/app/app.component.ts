4ffe1c011d871c51efbee03c7ffb6dba07f62d1d
zivkoss <zivcoss@yahoo.com>
2023-05-29 18:04:19 +0200

    997

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { Post } from './post.model'
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
-  isFatching = false;
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -40,7 +40,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
-    this.isFatching = true;
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>(
         'https://angular-http-basicsss-default-rtdb.firebaseio.com//posts.json'
@@ -57,7 +57,7 @@ export class AppComponent implements OnInit {
          })
         ) 
         .subscribe(posts => {
-          //  console.log(posts)
+          this.isFetching = false;
           this.loadedPosts = posts;
         });
 
