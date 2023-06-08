86c1598f0b9c72f630cff508b4538632f008154e
zivkoss <zivcoss@yahoo.com>
2023-06-08 10:28:03 +0200

    new code in post.service.ts added

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { Post } from './post.model';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
-   createAndStorePost(title: string, content: string) { 
-    // ...
+
+   constructor(private http: HttpClient) {}
+   
+   createAndStorePost(title: string, content: string) {  
+    const postData: Post = { title: title, content: content };
+    this.http
+    .post<{ name: string }>(
+    'https://angular-http-basicsss-default-rtdb.firebaseio.com//posts.json',
+     postData
+  )
+  .subscribe(responseData => {
+    console.log(responseData);
+  });
 } 
 
 fetchPoats() {
-    
+
    }
 }
