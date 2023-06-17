aa52dc83d90bb296b0e3e93c68261d1fb3807c07
zivkoss <zivcoss@yahoo.com>
2023-06-17 17:49:50 +0200

    coment added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,7 +42,7 @@ export class AppComponent implements OnInit {
   onClearPosts() {
     // Send Http request
     this.postsService.deletePosts().subscribe(() => {
-      this.loadedPosts = [];
+      this.loadedPosts = []; // **
     });
   }
 
