eb5cfacedc759bb99a9490b3be2b326dcaecd5b7
zivkoss <zivcoss@yahoo.com>
2023-07-11 15:58:47 +0200

    error = null; added on line 16, app.c.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ import { PostsService } from './posts.service';
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
+  error = null;
 
   constructor(private http: HttpClient, private postsService: PostsService) {}
 
@@ -36,6 +37,8 @@ export class AppComponent implements OnInit {
     this.postsService.fetchPosts().subscribe(posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
+    }, error => {
+
     });
   }
 
