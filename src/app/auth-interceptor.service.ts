cb38be39730abadd0d022bd350725fcf423a21ed
zivkoss <zivcoss@yahoo.com>
2023-07-12 16:37:37 +0200

    console.log('Request is on its way'); added in  auth-interceptor.service.ts

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -3,6 +3,7 @@ import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http'
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         console.log('Request is on its way');
+        const modifiedRequest = req.clone();
         return next.handle(req);
     }
 }  
