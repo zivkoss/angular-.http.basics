a273389eca3adebd6375f86f36f6ca784ece7864
zivkoss <zivcoss@yahoo.com>
2023-07-12 16:54:14 +0200

    console.log(req.url); line 6 in  auth-interceptor.service.ts

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -3,7 +3,8 @@ import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http'
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         console.log('Request is on its way');
+        console.log(req.url);
         const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')});
-        return next.handle(req);
+        return next.handle(modifiedRequest);
     }
 }  
